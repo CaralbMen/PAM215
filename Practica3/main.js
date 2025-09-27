d0051a71d752db0ba34de5293c4ff247545e1da1
Charlyy <124051193@upq.edu.mx>
2025-09-26 19:27:05 -0600

    Se realizó un export a la constante resta en utils.js, y se importó en main.js para mostrar la resta de diferentes numeros

---
+++ Practica3/main.js
@@ -0,0 +1,7 @@
+import {resta} from './utils.js';
+
+console.log(resta(10,5));
+console.log(resta(20,5));
+console.log(resta(15,3));
+console.log(resta(30,9));
+console.log(resta(4,1));
---
+++ Practica3/utils.js
@@ -0,0 +1,4 @@
+
+
+export const resta= (a,b)=> a-b
+
